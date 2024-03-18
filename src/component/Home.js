04f708b1bc10cb8f9060eebe3166356a16a8874e
최병우 <choebyeong-u@choebyeong-uui-MacBookPro.local>
2024-03-18 16:43:44 +0900

    f5 clear

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,9 +5,9 @@ import { BrowserRouter, Route, Link} from 'react-router-dom';
 const Home = (props) => {
     return (
         <div>
-            <h1><Link to="/test1">Test1</Link></h1>
-            <h1><Link to="/test2">Test2</Link></h1>
-            <h1><Link to="/test3">Test3</Link></h1>
+            <h1><Link to="/test1">Test1_가챠테스트</Link></h1>
+            <h1><Link to="/test2">Test2_연성글테스트</Link></h1>
+            <h1><Link to="/test3">Test3_AI 이미지 추출</Link></h1>
         </div>
     );
 };
